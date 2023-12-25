447c9484ad5ad23c81a9d022f10704f8ab97fb2f
Dadypool <dudypool@yandex.ru>
2023-12-25 16:31:19 +0300

    Add token

--- app/config.py
+++ app/config.py
@@ -7,11 +7,16 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    TOKEN: str
 
     @property
     def DATABASE_URL_psycopg(self):
         # postgresql+psycopg://postgres:postgres@localhost:5432/shop
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def Token(self):
+        return self.TOKEN
 
     model_config = SettingsConfigDict(env_file=".env")
 
