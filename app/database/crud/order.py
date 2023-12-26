849bb42d2415ad3475683a7e79e94ca7200475df
Dadypool <dudypool@yandex.ru>
2023-12-26 07:56:35 +0300

    Add new queries

--- app/database/crud/order.py
+++ app/database/crud/order.py
@@ -3,7 +3,7 @@ from pydantic import ValidationError
 
 from app.database.sqlalchemy import Session
 from app.database.models import Order, OrderStatus, Product
-from app.database.schemas import OrderCreate
+from app.database.schemas import OrderCreate, OrderSchema
 
 
 def create_order(order: dict) -> bool:
@@ -24,10 +24,18 @@ def create_order(order: dict) -> bool:
 
 
 def read_active_orders(user_id: int) -> list[dict]:
-    stmt = select(Order).where(Order.user_id == user_id).where(Order.status == Order.status.created or Order.status == Order.status.confirmed or Order.status == Order.status.shipped)
+    stmt = (
+        select(Order)
+        .where(Order.user_id == user_id)
+        .where(
+            Order.status == Order.status.created
+            or Order.status == Order.status.confirmed
+            or Order.status == Order.status.shipped
+        )
+    )
     with Session() as db:
         orders = db.scalars(stmt).all()
-    return orders
+    return [OrderSchema.from_orm(order).__dict__ for order in orders]
 
 
 def read_order_by_id(id: int) -> dict | None:
@@ -36,20 +44,43 @@ def read_order_by_id(id: int) -> dict | None:
         order = db.scalars(stmt).first()
     if not order:
         return None
-    return order
+    return OrderSchema.from_orm(order).__dict__
 
 
 def read_orders_by_user_id(user_id: int) -> list[dict]:
     stmt = select(Order).where(Order.user_id == user_id)
     with Session() as db:
         orders = db.scalars(stmt).all()
-    return orders
+    return [OrderSchema.from_orm(order).__dict__ for order in orders]
+
+
+def read_created_orders() -> list[dict]:
+    stmt = (
+        select(Order)
+        .where(Order.status == OrderStatus.created)
+        .order_by(Order.created_at)
+    )
+    with Session() as db:
+        orders = db.scalars(stmt).all()
+    return [OrderSchema.from_orm(order).__dict__ for order in orders]
+
+
+def read_order_price(order_id: int) -> int:
+    stmt = select(Order).where(Order.id == order_id)
+    with Session() as db:
+        order = db.scalars(stmt).first()
+        if not order:
+            return 0
+        price = 0
+        for product in order.products:
+            price += product.price
+    return price
 
 
 def update_order_status(id: int, new_status: OrderStatus) -> bool:
     with Session() as db:
         order = db.get(Order, id)
-        if not order:
+        if not order or order.status == "cart":
             return False
         order.status = new_status
         db.commit()
